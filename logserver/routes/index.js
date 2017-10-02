166ac6c5da719a8298b8769a35ffa55ebe2c50da
Daniele De Francesco <daniele@de-francesco.it>
2017-10-02 18:27:49 -0400

    initial commit

---
+++ logserver/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/weather', function(req, res, next) {
+  fs.readFile('item.txt', function(err, data) {
+    if (err) {res.send(err);}
+    else {
+      res.send(data);
+    }
+  })
+  //res.render('index', { title: 'Express' });
+});
+
+router.post('/weather', addItem);
+
+module.exports = router;
+
+function addItem(req, res) {
+  var item = req.body;
+  for (var prop in item) {
+    //console.log('adding ' + prop + ' with ' + item[prop]);
+    fs.appendFileSync(prop + '.txt', item[prop] + '\n');
+  }
+  res.sendStatus(200);
+}
+
